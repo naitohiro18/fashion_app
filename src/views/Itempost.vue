fb3b1c38ea8a24c9953693bfd55b41ac916481f6
taiga75 <ta.nono0529@gmail.com>
2020-09-06 18:00:00 +0900

    unnti

--- src/views/Itempost.vue
+++ src/views/Itempost.vue
@@ -1,9 +1,62 @@
 <template>
-  <h1>post</h1>
+  <div class="post">
+    <select name="category">
+      <option value>カテゴリー</option>
+      <option value="トップス">トップス</option>
+      <option value="ボトムス">ボトムス</option>
+      <option value="シューズ">シューズ</option>
+    </select>
+
+    <select name="itemsize">
+      <option value>サイズ</option>
+      <option value="S">S</option>
+      <option value="M">M</option>
+      <option value="L">L</option>
+      <option value="LL">LL</option>
+    </select>
+
+    <input type="file" @change="onImageUploaded" style="display: none;" />
+    <input type="submit" name="botton" value="送る" />
+    <img
+      id="image"
+      src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5
+      YII="
+    />
+    <input type="file" @change="onImageUploaded" />
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      submittedArticle: {
+        title: "",
+        description: "",
+        image: null
+      }
+    };
+  },
+  methods: {
+    onImageUploaded(e) {
+      // event(=e)から画像データを取得する
+      var image2 = document.getElementById("image");
+      image2.src = window.URL.createObjectURL(e.target.files[0]);
+      const image = e.target.files[0];
+      console.log(image);
+      this.createImage(image);
+    },
+    createImage(image) {
+      const reader = new FileReader();
+      // imageをreaderにDataURLとしてattachする
+      reader.readAsDataURL(image);
+      // readAdDataURLが完了したあと実行される処理
+      reader.onload = () => {
+        this.submittedArticle.image = image;
+      };
+    }
+  }
+};
 </script>
 
 <style>
