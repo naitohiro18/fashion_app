a039d64fe4ea2b1970b447c4d5e89e25d8f74b83
photographerori <takataka.malong@gmail.com>
2020-09-06 14:34:23 +0900

    rewrite

--- src/store/index.js
+++ src/store/index.js
@@ -4,25 +4,25 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {//dataの保存場所
-    user:null,
+  state: {
+    //dataの保存場所
+    user: null,
   },
-  mutations: {//stateの情報をいじる
-    addUser(state, user){
-      state.user=user
-    }
+  mutations: {
+    //stateの情報をいじる
+    addUser(state, user) {
+      state.user = user;
+    },
   },
   actions: {
-  // addUser(){
-     //mutationsと連携
-// increment(context, payload){
-  //context.commit('increment', payload);
-  addUser(context,user){
-    context.commit('addUser',user);
-  }
-   },
+    // addUser(){
+    //mutationsと連携
+    // increment(context, payload){
+    //context.commit('increment', payload);
+    addUser(context, user) {
+      context.commit("addUser", user);
+    },
+  },
   modules: {},
-  getters:{
-    
-  }
+  getters: {},
 });
