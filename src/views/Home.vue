04e925beb83f6b352d8398c4d799a425879def4a
taiga75 <ta.nono0529@gmail.com>
2020-09-13 23:03:50 +0900

    minimum

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,16 @@
 <template>
   <div class="home">
     <Header></Header>
-<<<<<<< HEAD
-    <nav class="navigation">
-      <a class="navigation-item" href="#">すべて</a>
-      <a class="navigation-item" href="#">トップス</a>
-      <a class="navigation-item" href="#">パンツ</a>
-      <a class="navigation-item" href="#">アウター</a>
-      <a class="navigation-item" href="#">シューズ</a>
-      <a class="navigation-item" href="#">バッグ</a>
-    </nav>
-
-    <div v-if="isUploaded">
-      <p>洋服を追加してみよう。</p>
-      <img class="shoes" src="@/assets/shoes.png" alt="シューズの画像" />
-    </div>
+    <!-- <Navigation></Navigation> -->
+    <p v-if="!this.clothes">洋服を追加してみよう。</p>
     <div v-else>
-      <!-- firebaseから取得してここにアイテムの画像などの一覧を表示する -->
+      <div v-for="cloth in clothes" :key="cloth.id">
+        <img v-bind:src="cloth.item.image_url" alt="服の写真" />
+        <span>{{ cloth.item.category }}</span>
+        <span>{{ cloth.item.itemsize }}</span>
+      </div>
     </div>
-=======
-    <Navigation></Navigation>
-    <p>洋服を追加してみよう。</p>
     <img class="shoes" src="@/assets/shoes.png" alt="シューズの画像" />
->>>>>>> origin/feature/itemaddTop
     <Footer></Footer>
   </div>
 </template>
@@ -31,14 +19,36 @@
 // @ is an alias to /src
 
 import Header from "../components/header.vue";
-import Navigation from "../components/Navigation.vue";
+// import Navigation from "../components/Navigation.vue";
 import Footer from "../components/footer.vue";
+import { db } from "@/main";
 export default {
   name: "Home",
+  data() {
+    return {
+      clothes: null
+    };
+  },
   components: {
     Header,
-    Footer,
-    Navigation
+    Footer
+    // Navigation
+  },
+  mounted() {
+    db.collection("items")
+      .get()
+      .then(snapshot => {
+        this.clothes = snapshot.docs.map(doc => {
+          return {
+            id: doc.id,
+            ...doc.data()
+          };
+        });
+      })
+      .catch(err => {
+        console.log("Error getting documents", err);
+      });
+    // console.log(this.clothes[0].item.itemsize);
   }
 };
 </script>
